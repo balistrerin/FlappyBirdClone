67c7454583c503e9a5b67ca2c84433e5daa121b3
Nick Balistreri <balistrerin@gmail.com>
2016-04-03 11:21:20 -0700

    Loaded Sprites, Font , and Flap animation

---
+++ ClonyBird/Assets/BirdMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+    Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
